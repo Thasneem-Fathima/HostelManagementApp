ec7400411ac3a5b61e9e495e7707763ff47ce0a0
Thasneem Fathima <thasneemf78@gmail.com>
2023-11-28 18:41:14 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="/slogin">
+      <Button variant="contained" color="primary" size="medium" style={{ marginTop: '50px',textTransform: 'none' ,fontSize:'20px' }}>
+        Student Login
+      </Button></Link>
+      <Link to="/tlogin">
+         <Button variant="contained" color="primary" size="medium" style={{ marginTop: '50px',marginLeft:'100px',textTransform: 'none' ,fontSize:'20px' }}>
+        Staff Login
+      </Button>
+      </Link>
     </div>
   );
 }
